aceb332e3d725c6f4528766268545d11832e706a
Jofi <103282270+jofizcd@users.noreply.github.com>
2024-08-03 22:19:29 +0500

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 call python -m venv data
 call data\Scripts\activate
 pip install torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu118
-pip install pygame aiohttp aiofiles translators 
+pip install pygame aiohttp aiofiles translators PyAudio
 pip install SpeechRecognition soundfile elevenlabs qasync silero_tts characterai
 pip install PyQt6 PySide6 numpy==1.26.4
 python main.py
