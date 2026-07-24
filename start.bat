53b7e175284a57beef21e4901d32ed92f1a554fa
Jofi <103282270+jofizcd@users.noreply.github.com>
2026-07-25 02:14:58 +0500

    Version 2.4.0 - Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
-title Soul of Waifu v2.3.1
+title Soul of Waifu v2.4.0
 color 0A
 
 echo =============================================
-echo  Soul of Waifu v2.3.1 - Starting Application
+echo  Soul of Waifu v2.4.0 - Starting Application
 echo =============================================
 echo.
 
